c19ed55b605b6e1ff4ab0344a40f1ea3af07b312
Ibrahim <ibamibrahim0@gmail.com>
2017-05-16 10:48:00 +0700

    add model class (.java) for search results items

---
+++ app/src/main/java/com/bukantkpd/bukabareng/SearchResultsItems.java
@@ -0,0 +1,44 @@
+package com.bukantkpd.bukabareng;
+
+/**
+ * Created by Ibam on 5/16/2017.
+ */
+
+public class SearchResultsItems {
+
+   private String productName;
+    private String productGroceryPrice;
+    private String productNormalPrice;
+    private String productCurrentQtyBuying;
+    private int productImage;
+
+    public SearchResultsItems(String productName, String productGroceryPrice, String productNormalPrice, String productCurrentQtyBuying, int productImage) {
+        this.productName = productName;
+        this.productGroceryPrice = productGroceryPrice;
+        this.productNormalPrice = productNormalPrice;
+        this.productCurrentQtyBuying = productCurrentQtyBuying;
+        this.productImage = productImage;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductGroceryPrice() {
+        return productGroceryPrice;
+    }
+
+    public String getProductNormalPrice() {
+        return productNormalPrice;
+    }
+
+    public String getProductCurrentQtyBuying() {
+        return productCurrentQtyBuying;
+    }
+
+    public int getProductImage() {
+        return productImage;
+    }
+
+
+}
