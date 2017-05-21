e7e3682b16037c0260a7a00c5cd15d2061a24cd8
Ibrahim <ibamibrahim0@gmail.com>
2017-05-21 21:16:54 +0700

    add login and splash screen

---
+++ app/src/main/java/com/bukantkpd/bukabareng/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.bukantkpd.bukabareng;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.login_button_view);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.login_button_view:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                Log.d("DEBUG"," Login pressed");
+                break;
+        }
+    }
+}
