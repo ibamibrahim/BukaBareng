915be2ae7642bb01de6907be5b98d361b2d2b6ec
Ibrahim <ibamibrahim0@gmail.com>
2017-05-25 23:39:53 +0700

    add getproductsearch method in interface

--- app/src/main/java/com/bukantkpd/bukabareng/api/remote/BukBarAPIService.java
+++ app/src/main/java/com/bukantkpd/bukabareng/api/remote/BukBarAPIService.java
@@ -4,11 +4,14 @@ package com.bukantkpd.bukabareng.api.remote;
  * Created by Ibam on 5/25/2017.
  */
 
+import com.bukantkpd.bukabareng.api.model.SearchResultListModel;
 import com.bukantkpd.bukabareng.api.model.UserModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -16,5 +19,7 @@ public interface BukBarAPIService {
 
     @POST("/auth/ret_user")
     Call<UserModel> getUsersDetail(@Query("email") String email, @Query("password") String password);
-    
+
+    @GET("/get/products")
+    Call<SearchResultListModel> getProductSearch(@Query("keyword") String keyword, @Query("token") String token);
 }
