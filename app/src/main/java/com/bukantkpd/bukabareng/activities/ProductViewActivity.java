ed375f4ec88335d84b1fe91d54b2bff04a07522b
Ibrahim <ibamibrahim0@gmail.com>
2017-05-28 10:49:31 +0700

    add buy dialog

--- app/src/main/java/com/bukantkpd/bukabareng/activities/ProductViewActivity.java
+++ app/src/main/java/com/bukantkpd/bukabareng/activities/ProductViewActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.media.Image;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bukantkpd.bukabareng.R;
 import com.bukantkpd.bukabareng.api.model.ProductModel;
+import com.bukantkpd.bukabareng.api.model.UserDetailModel;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
@@ -38,18 +42,24 @@ public class ProductViewActivity extends AppCompatActivity {
     TextView viewDeadline;
     TextView viewDescription;
     ImageView viewImage;
-
+    Button buyButton;
     @Override
     @SuppressWarnings("deprecation")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_view);
+        Gson gson = new Gson();
 
         isLoggedIn();
 
-        Gson gson = new Gson();
+        SharedPreferences preferences = getSharedPreferences("bukabareng",
+                Context
+                        .MODE_PRIVATE);
+        String userObj = preferences.getString("userDetail", null);
+        final UserDetailModel user = gson.fromJson(userObj, UserDetailModel.class);
+
         String strObj = getIntent().getStringExtra("product");
-        ProductModel item = gson.fromJson(strObj, ProductModel.class);
+        final ProductModel item = gson.fromJson(strObj, ProductModel.class);
 
         String productName= item.getName();
         String normalPrice = NumberFormat.getNumberInstance(Locale.GERMAN).format(item
@@ -103,6 +113,49 @@ public class ProductViewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        buyButton = (Button) findViewById(R.id.buy_button_product_view);
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ProductViewActivity
+                        .this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_buy, null);
+                final EditText quantity = (EditText) dialogView.findViewById(R.id.quantitiy_buy);
+                Button buyButtonFinal = (Button) dialogView.findViewById(R.id.buy_button_final);
+
+                dialogBuilder.setView(dialogView);
+                final AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+
+                buyButtonFinal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String quantityStr = quantity.getText().toString();
+                        if(quantityStr.equals("")){
+                            Toast.makeText(ProductViewActivity.this, "Jumlah tidak boleh kosong!",
+                                    Toast
+                                    .LENGTH_SHORT).show();
+                        } else {
+                            try {
+                                int quantityInt = Integer.parseInt(quantityStr);
+                                String productID = item.getProductId();
+                                String userID = user.getUserId() + "";
+                                String belibarengID = item.getMassdropId() + "";
+
+
+                            } catch (NumberFormatException e){
+                                Toast.makeText(ProductViewActivity.this, "Masukkan jumlah dalam " +
+                                        "bilangan bulat!", Toast.LENGTH_SHORT).show();
+                            }
+
+                            }
+                    }
+                });
+
+
             }
         });
     }
